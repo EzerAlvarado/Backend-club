33436f99c75d7b25eea227207a6214904bfb7dcc
Ezer Jehu Alvarado <ezer.alvarado@sigob.com.mx>
2025-02-20 11:11:51 -0700

    Removiendo carpetas __pycache__ y entornos virtuales

--- club/serializers/cargos.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from club.models import Cargo
-
-class CargoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cargo
-        fields = ['id', 'total_cobro', 'fecha_cobro', 'usuario_responsable', 'estado', 'mesa']
