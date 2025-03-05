c1b444ef71bb473f836d1f6ff78e4239ac8c63f7
Ezer Jehu Alvarado <ezer.alvarado@sigob.com.mx>
2025-03-05 09:52:25 -0700

    Updated admin.py

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from club import models
+
+
+admin.site.register(models.Bloque)
+admin.site.register(models.Cliente)
+admin.site.register(models.Corte)
+admin.site.register(models.Evento)
+admin.site.register(models.Ganancia)
+admin.site.register(models.Inventario)
+admin.site.register(models.Mesa)
+admin.site.register(models.OrdenDeCompra)
+admin.site.register(models.Producto)
+admin.site.register(models.Ticket)
+admin.site.register(models.Usuario)
+admin.site.register(models.Producto)
+admin.site.register(models.PrecioBloque)
+admin.site.register(models.Cargo)
